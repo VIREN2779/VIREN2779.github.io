24eb6e241494976ab51633ac6ed79cf417180d02
Viren Patadiya <102893049+VIREN2779@users.noreply.github.com>
2026-01-29 09:58:16 +0000

    feat(script.js): menu items close after click

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const toggle = document.getElementById("toggle");
 const menu = document.getElementById("menu");
+const links = document.querySelectorAll("#menu a");
 
 toggle.addEventListener("click", () => {
     menu.classList.toggle("show");
+});
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        menu.classList.remove("show");
+    });
 });
 
 document.getElementById("contactForm").addEventListener("submit", e => {
